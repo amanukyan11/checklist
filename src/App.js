20bf9e9253430b20cb5b781a2560a55f6a9b15f9
Raine Soriano <raine.a.soriano@gmail.com>
2023-02-13 18:00:42 -0800

    Static main page added, with subcomponents to be implemented with interactivity.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React, { Component } from "react";
 import Login from './login'
+import Main from './main'
  
 class App extends Component {
   render() {
     return (
       <div>
         <Login />
+        <Main />
       </div>
     );
   }
