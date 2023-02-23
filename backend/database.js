914088ca69db0b458d7613d4d076f40326369a39
Raine Soriano <raine.a.soriano@gmail.com>
2023-02-22 23:01:00 -0800

    DB Query: getUserInfo, createChecklist; db_test.js
    database.js changed output of previous two DB query functions
    db_test.js contains test cases for database.js

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const Client = require('pg').Client;
 const R = require('ramda');
 
 // export function for use in backend code
-module.exports = {authenticateUser, addUser};
+module.exports = {authenticateUser, addUser, getUserInfo, createChecklist};
 
 // information to connect to the pgsql server
 const clientInfo = ({
@@ -19,44 +19,117 @@ const clientInfo = ({
 async function addUser(username, password) {
 	// query to send posgreSQL server
 	const txt = `INSERT INTO user_info (email, password) 
-				VALUES ($1, crypt($2, gen_salt('bf')))`;
-	const vals = [username, password]
+				VALUES ($1, crypt($2, gen_salt('bf')))
+				RETURNING userID`;
+	const vals = [username, password];
 	// connect to server and send query
 	// returns a promise that resolves into whether the user exists or not
 	
     const client = new Client(clientInfo);
-	client.connect();
-	
+	await client.connect();
+	let userID = null;
     try {
 		const res = await client.query(txt, vals);
-	await client.end();
-	return { "status": 0 };
+		userID = res.rows[0];
 	} catch (err) {
-		console.log(err.stack);
+		console.log(err.detail);
+	} finally {
+		await client.end();
+		return { "userid": userID };
 	}
-	client.end();
-	return { "status": 1 };
+	
 }
 
 async function authenticateUser(email, passwordAttempt) {
 	const txt = `SELECT userID
 	FROM user_info
 	WHERE email=$1
-	AND password=crypt($2,password)`
-	const vals = [email, passwordAttempt]
+	AND password=crypt($2,password)`;
+	const vals = [email, passwordAttempt];
 
 	const client = new Client(clientInfo);
-	client.connect();
-	
+	await client.connect();
+	let userID = null;
     try {
 		const res = await client.query(txt, vals);
 		await client.end();
-		return { "userID": res.rows[0] };
+	
+		if (res.rows.length){
+			userID = res.rows[0]["userid"];
+		}
 	} catch (err) {
 		console.log(err.stack);
+	} finally {
+		await client.end();
+		return { "userid" : userID};
 	}
-	client.end();
-	return { "userID": null };
+	
+}
+
+async function getUserInfo(userID) {
+	const listsQuery = `
+		SELECT checklists.listID, checklists.name, checklists.content, checklists.checked
+		FROM checklists
+		JOIN perms
+		ON checklists.listID = perms.listID
+		WHERE perms.userID::text = $1
+	`;
+	const userQuery = `
+		SELECT email, tree_prog
+		FROM user_info
+		WHERE userID::text = $1
+	`;
+	const values = [userID];
+	
+
+	const client = new Client(clientInfo);
+	await client.connect();
+	let lists;
+	let userInfo;
+	try {
+		await client.query(listsQuery, values).then(res => lists = res.rows);
+		await client.query(userQuery, values).then(res => userInfo = res.rows);
+	} catch (err) {
+		console.log(err)
+	} finally {
+		await client.end();
+	}
+
+	if (!userInfo.length) {
+		return { "status": 1};
+	}
+	const ret = {
+		...userInfo[0],
+		"lists": lists
+	}
+	return ret;
+}
+
+async function createChecklist(userID, listName, newContent, newChecked) {
+
+	const query = `
+		with id as (INSERT INTO checklists (name, content, checked) 
+		VALUES ($2, $3, $4)
+		RETURNING listID)
+
+		INSERT INTO perms (listID, userID) 
+		VALUES ((select * from id), $1)
+		RETURNING listID
+	`;
+	const values = [userID, listName, newContent, newChecked];
+
+	const client = new Client(clientInfo);
+	await client.connect();
+	let listID = null
+	try {
+		await client.query(query, values).then(res => listID = res.rows[0]["listid"]);
+	} catch (e) {
+		console.log(e.stack);
+	} finally {
+		await client.end();
+		return { "listid": listID };
+	}
+
 }
 
 /*
