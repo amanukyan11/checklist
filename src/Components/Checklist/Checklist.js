30e5b8b76d453d836c04516d93d453c2cedfdcf4
amanukyan11 <97646702+amanukyan11@users.noreply.github.com>
2023-02-23 17:53:44 -0800

    Added 'Save' button, edited counters functionality

--- src/Components/Checklist/Checklist.js
+++ src/Components/Checklist/Checklist.js
@@ -5,6 +5,7 @@ function Checklist() {
   const [tasks, setTasks] = useState([]); //defines a variable tasks and a function setTask that updates using 'useState'
   const [numberOfTasks, setNumberOfTasks] = useState(0); //used to incrament the number of tasks that have been created.
   const [completedTasks, setCompleteTasks] = useState(0);
+  const [isTextBoxActive, setTextBoxActive] = useState(false);
 
   const addTask = () => {   //adds a new task to the 'task' array and uses setTasks to update the 'task'
     //TODO #1: check if the current task value is not empty
@@ -13,11 +14,9 @@ function Checklist() {
   };
 
   function incramentTasks () {
-    setNumberOfTasks(numberOfTasks + 1);
-  }
-
-  function incramentCompletedTasks () {
-    setCompleteTasks(completedTasks + 1);
+    if (isTextBoxActive) {
+      setNumberOfTasks(numberOfTasks + 1);
+    }
   }
 
   const updateTask = (index, value) => { //takes in an index and value and creates a new array by copying the current 'task' array. Then newTasks is updated and set with the new index and value.
@@ -29,7 +28,9 @@ function Checklist() {
   const removeTask = (index) => { //remove tasks by copying the 'tasks' array into newTasks then using the .splice function the item from the list based on the index is removed. 'tasks' is updated
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
-    setNumberOfTasks(numberOfTasks - 1); 
+    if (numberOfTasks !== 0) {
+      setNumberOfTasks(numberOfTasks - 1); 
+    }
     setTasks(newTasks);
   };    
 
@@ -59,6 +60,7 @@ function Checklist() {
             type="text"
             value={task.text}
             onChange={(event) => updateTask(index, {text: event.target.value, isCompleted: false})}
+            onFocus={() => setTextBoxActive(true)}
           />
           {task.isCompleted ? (
           <button className="checkButton" onClick={() => crossOutTask(index)}>&#10003;</button>
@@ -68,11 +70,15 @@ function Checklist() {
         </div>
       ))}
       <form onSubmit={handleSubmit}>
-        <button className="newTask" type="submit" onClick={incramentTasks}>Add Task</button>
+        <button className="newTask" type="submit">Add Task</button>
+        {isTextBoxActive && (
+        <button className="saveButton" onClick={() => {
+          setTextBoxActive(false)
+          incramentTasks();
+          }}>Save</button>)}
       </form>
       <div>
     <p>Number of tasks: {numberOfTasks} Number of completed tasks: {completedTasks}</p>
-    {/* Rest of your JSX */}
   </div>
     </div>
   );
@@ -81,6 +87,17 @@ function Checklist() {
 export default Checklist;
 
 
-//Add a save button
-//Be able to un-check box
-//*add a task counter 
+/*Add a save button 
+  -when text box is clicked, add a new button next to 'Add Task' DONE
+  -Only save and update number of tasks counter when 'save' button is clicked DONE
+*/
+//Be able to un-check box DONE
+//add a task counter DONE
+
+/*ISSUES
+-Should not be able to click "Add Task" if the 'save' button has not been clicked AND the text box must be filled with something
+-When a completed task is removed, decrease "completedTask" counter
+-Cannot click check box if task is empty
+-If text box is empty, clicking save should NOT updated "numberOfTasks"
+-If you already have a task in text box and you have clicked 'Save' updating the "numberOfTasks" counter, editing it and clicking save should NOT increase counter.
+*/
