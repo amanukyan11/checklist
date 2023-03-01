3ddc3944778df3971748e9a3aae0cd5335cb3e25
Aster Phan <phanaster7@gmail.com>
2023-02-28 16:01:19 -0800

    Add code for backend server and functions for frontend to call backend.
    Backend server has been tested and should now be completed.
    Frontend can import connect.js and use the functions inside it to
    interact with the backend.

--- src/connect.js
+++ src/connect.js
@@ -1,29 +1,86 @@
-async function addUser(username, password) {
+module.exports = {authenticateUser, addUser, getUserInfo, createChecklist, updateChecklist, shareList, growTree, deleteChecklist, deleteUser};
 
-	const res = await fetch(`http://localhost:5000/addUser/${username}/${password}`);
-	
-	const ret = await res.json();
-
-	console.log(ret);
-
-	return ret["status"] === 0;
+async function addUser(email, password) {
+	const res = await fetch(`http://localhost:5000/addUser/${email}/${password}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
 }
+
+
+async function authenticateUser(email, password) {
+	const res = await fetch(`http://localhost:5000/authenticateUser/${email}/${password}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function getUserInfo(userID) {
+	const res = await fetch(`http://localhost:5000/getUserInfo/${userID}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function createChecklist(userID, listName, newContent, newChecked) {
+	const content = JSON.stringify(newContent);
+	const checked = JSON.stringify(newChecked);
+	const res = await fetch(`http://localhost:5000/createChecklist/${userID}/${listName}/${content}/${checked}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function updateChecklist(listID, listName, newContent, newChecked) {
+	const content = JSON.stringify(newContent);
+	const checked = JSON.stringify(newChecked);
+	const res = await fetch(`http://localhost:5000/updateChecklist/${listID}/${listName}/${content}/${checked}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function shareList(listID, newEmail) {
+	const res = await fetch(`http://localhost:5000/shareList/${listID}/${newEmail}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function growTree(userID, amount) {
+	const res = await fetch(`http://localhost:5000/growTree/${userID}/${amount}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function deleteChecklist(listID) {
+	const res = await fetch(`http://localhost:5000/deleteChecklist/${listID}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
+async function deleteUser(userID) {
+	const res = await fetch(`http://localhost:5000/deleteUser/${userID}`);
+	const response = await res.text();
+	const ret = JSON.parse(response);
+	return ret;
+}
+
 
 /*
 
-test code
+example code
 
-// evaluates to true
-addUser('aster', 'pass123!').then((val) => console.log(val));
-
-// evaluates to false
-addUser('aster', 'pass123!').then((val) => console.log(val));
-
-// evaluates to false
-addUser('aster', 'aaaa!').then((val) => console.log(val));
-
-// evaluates to true at first, then false onwards
-addUser('isuehnbesksegytuer', 'aaaa!');
-
+addUser('testing@blah.com', 'pass123!').then((val) => console.log(val));
+authenticateUser('testing@blah.com', 'pass123!').then((val) => console.log(val));
+authenticateUser('testing@blah.com', 'aaaaaa!').then((val) => console.log(val));
+getUserInfo('9b692ff7-c2f0-46a6-86fd-0316ca57bf9b').then((val) => console.log(val));
+getUserInfo('9b692ff7-c2f0-46a6-86fd-0316ca57bf9b').then((val) => console.log(val["lists"]));
+createChecklist('9b692ff7-c2f0-46a6-86fd-0316ca57bf9b', 'sendhelp', ["cat", "dog"], [true, false]).then((val) => console.log(val));
+shareList('1b5b6893-b777-4fd2-b5bd-2b367ca960fd', 'jiho@mail.com').then((val) => console.log(val));
+growTree('9b692ff7-c2f0-46a6-86fd-0316ca57bf9b', 99).then((val) => console.log(val));
+deleteUser('9b692ff7-c2f0-46a6-86fd-0316ca57bf9b').then((val) => console.log(val));
 
 */
