bae0e174267290c716d7979f1421f67617382756
hovik16 <hovik16@gmail.com>
2023-03-08 14:36:13 -0800

    Refactored MainPage to have the menu render the checklist and progress bar instead
    - realized that the Menu has to be in charge of rendering the checklist, not the main page
    - removed all code that mentioned the checklist and progress bar from the mainpage files, then moved that code over to the menu files
    - formatted menu to have control over the checklist and progressbar
    - the checklist and progressbar files still contain the code for their respective functions, they're just now rendered using the menu instead of the mainpage

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,27 +1,13 @@
 import "./MainPage.css"
-import ProgressBar from '../ProgressBar/ProgressBar';
 import Timer from '../Timer/Timer';
 import Tree from '../Tree/Tree';
-import Checklist from "../Checklist/Checklist"
 import Menu from '../Menu/Menu';
 function MainPage () {
     return (
         <div className="MainPage">
             <div className="container">
                 <div className="mainLeft">
-                    <h1 className="leftTop">
-                        <div className="titles">Menu</div>
-                    </h1>
                     <Menu/>
-                </div>
-                <div className="mainCenter">
-                    <h1 className="centerTop">
-                        <div className="titles">Checklist</div>
-                    </h1>
-                    <Checklist />
-                    <footer className="centerBottom">
-                        <ProgressBar/>
-                    </footer>
                 </div>
                 <div className="mainRight">
                     <h1 className="topRight">
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,14 +1,36 @@
 import "./Menu.css"
+import Checklist from "../Checklist/Checklist"
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 function Menu () {
-    return <div className="menuList">
-        <div className="lists">
-            <button className="listNumber">List 1</button>
+    return( 
+    <div className="overall">
+        <div className="menuSection">
+            <h1 className="leftTop">
+                <div className="titles">Menu</div>
+            </h1>
+            
+            <div className="menuList">
+                <div className="lists">
+                    <button className="listNumber">List 1</button>
+                </div>
+                <div>
+                    <button className="addList">New List</button>
+                </div>
+            </div>
         </div>
-        <div>
-            <button className="addList">New List</button>
+
+        <div className="checklistSection"> 
+            <h1 className="centerTop">
+                <div className="titles">Checklist</div>
+            </h1>
+            <Checklist />
+            <footer className="centerBottom">
+                <ProgressBar/>
+            </footer>
         </div>
     </div>
+    )
 }
 
 export default Menu
