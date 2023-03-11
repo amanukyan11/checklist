cee0cd97ed041992a81d6a1a8cadd6add12fef5f
Christopher Gharghour <christopher.gharghour@Christophers-MacBook-Air.local>
2023-03-10 18:45:52 -0800

    implemented a basic version of tree feature activated by click as first step to full implementation

--- src/Components/Tree/Tree.js
+++ src/Components/Tree/Tree.js
@@ -1,11 +1,73 @@
+import Class1 from "./images/photo1.png";
+import Class2 from "./images/photo2.png";
+import Class3 from "./images/photo3.png";
+import Class4 from "./images/photo4.png";
+import Class5 from "./images/photo5.png";
 import "./Tree.css"
+import React, { useState } from "react";
 
 function Tree () {   
+    let img = null;
+    const [active, setActive] = useState(0);
+  
+    const handleChangeActive = () => {
+    setActive((previousStar) => (previousStar + 1) % 5);
+};
+
+    if (active===0) {
+      img = (
+        <img
+          className="photo1"
+          src={Class1}
+          alt="photo 1"
+          onClick={() => handleChangeActive()}
+        />
+      );
+    } else if (active===1) {
+      img = (
+        <img
+          className="photo2"
+          src={Class2}
+          alt="photo 2"
+          onClick={() => handleChangeActive()}
+        />
+      );
+    } else if (active===2) {
+      img = (
+        <img
+          className="photo3"
+          src={Class3}
+          alt="photo 3"
+          onClick={() => handleChangeActive()}
+        />
+      );
+    } else if (active===3) {
+      img = (
+        <img
+          className="photo4"
+          src={Class4}
+          alt="photo 4"
+          onClick={() => handleChangeActive()}
+        />
+      );
+    } else if (active===4) {
+      img = (
+        <img
+          className="photo5"
+          src={Class5}
+          alt="photo 5"
+          onClick={() => handleChangeActive()}
+        />
+      );
+    } 
+  
     return (
-    <div className="Tree">
-        <img src='https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576847__480.png' alt="tree"></img> 
-    </div>
-    )
+      <>
+        <div className="toggle-wrapper">
+          {img}
+        </div>
+      </>
+    );
 }
 
-export default Tree
+export default Tree;
