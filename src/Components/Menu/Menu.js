c792d8de1db74d3d35ea15e4ee438be2b46ba0d9
Christopher Gharghour <christopher.gharghour@Christophers-MacBook-Air.local>
2023-03-13 20:03:11 -0700

    Added features to menu like remove

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -2,23 +2,44 @@ import "./Menu.css"
 import Checklist from "../Checklist/Checklist"
 import ProgressBar from '../ProgressBar/ProgressBar';
 import {useState} from 'react';
-
+//Test
 function Menu (props) {
-    const [lists, setLists] = useState(props.lists); 
+    const [lists, setLists] = useState(props.lists.map(list => ({ checklist: list, value: 0 })));
     const [currIndex, setCurrIndex] = useState(0);
+    const [isSelected, setIsSelected] = useState(false);
 
     const addList = () => {
         setLists([...lists, '']);
     }
     
-    //const removeList() {} this is to be done later
-
-    const handleSubmit = (event) => { //This is the 'New List' button's functionality.
-        event.preventDefault();
-        addList();
-
+    const removeList = (index) => {//this removes a list from the menu
+        const newLists = [...lists];
+        newLists.splice(index, 1);
+        setLists(newLists);
     };
 
+    const handleListClick = (index) => { //this handles the assigning of isSelected for switching from dot to empty
+        setCurrIndex(index);
+        setLists(prevLists => prevLists.map((list, i) => {
+          if (i === index) {
+            return { ...list, isSelected: true };
+          } else {
+            return { ...list, isSelected: false };
+          }
+        }));
+      };
+    
+    const handleSubmit = (event) => { //This is the 'Add Task' button's functionality.
+        event.preventDefault();
+        addList();
+    };
+      
+    //checklist accept props, 
+
+    //List 1 (0) --> checklist --> lists array (0)
+    //List 2 (1) --> checklsit --> lists array (1)
+    
+    //add a delete button, and make the selected list have a "o"
     return( 
     <div className="overall">
         <div className="menuSection">
@@ -27,8 +48,20 @@ function Menu (props) {
             </h1>            
             <div className="menuList">
                 {lists.map((list, index) => (
-                    <div className="lists">
-                        <button className="listNumber" onClick={() => setCurrIndex(index)}>List {index + 1}</button>
+                    <div className="lists" key={index}>
+                      <button className="removeList" onClick={() => removeList(index)}>X</button>
+                      {/* <button className="listNumber" onClick={() => handleListClick(index)}>List {index + 1}</button> */}
+                      <input 
+                        className="listNumber" 
+                        type="text" 
+                        value={`List ${index + 1}`} 
+                        onChange={() => setCurrIndex(index)} 
+                      />
+                      {list.isSelected ? (
+                        <button className="selectedButton" onClick={() => handleListClick(index)}>•</button>
+                    ) : (
+                            <button className="emptyButton" onClick={() => handleListClick(index)}>&shy;</button>
+                        )}
                     </div>
                 ))}
                 <div>
