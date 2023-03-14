fc618122c7dc33484960209d3f38be398beb06ba
Christopher Gharghour <christopher.gharghour@Christophers-MacBook-Air.local>
2023-03-13 19:20:08 -0700

    updated menu.js

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -2,22 +2,40 @@ import "./Menu.css"
 import Checklist from "../Checklist/Checklist"
 import ProgressBar from '../ProgressBar/ProgressBar';
 import {useState} from 'react';
-//Test
+
 function Menu (props) {
-    const [lists, setLists] = useState(props.lists);
+    const [lists, setLists] = useState(props.lists); 
+    const [currIndex, setCurrIndex] = useState(0);
+
+    const addList = () => {
+        setLists([...lists, '']);
+    }
+    
+    //const removeList() {} this is to be done later
+
+    const handleSubmit = (event) => { //This is the 'New List' button's functionality.
+        event.preventDefault();
+        addList();
+
+    };
+
     return( 
     <div className="overall">
         <div className="menuSection">
             <h1 className="leftTop">
                 <div className="titles">Menu</div>
-            </h1>
-            
+            </h1>            
             <div className="menuList">
-                <div className="lists">
-                    <button className="listNumber">List 1</button>
-                </div>
+                {lists.map((list, index) => (
+                    <div className="lists">
+                        <button className="listNumber" onClick={() => setCurrIndex(index)}>List {index + 1}</button>
+                    </div>
+                ))}
                 <div>
-                    <button className="addList">New List</button>
+                    <form onSubmit={handleSubmit}>
+                        <button className="addList" type="submit">New List</button>
+                        <button>{currIndex}</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -26,7 +44,7 @@ function Menu (props) {
             <h1 className="centerTop">
                 <div className="titles">Checklist</div>
             </h1>
-            <Checklist />
+            <Checklist list={lists[currIndex]}/>
             <footer className="centerBottom">
                 <ProgressBar/>
             </footer>
@@ -34,5 +52,3 @@ function Menu (props) {
     </div>
     )
 }
-
-export default Menu
