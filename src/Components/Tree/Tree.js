02b32fd7dc4cbaa07f35646ae9dea768f70edf15
Raine Soriano <raine.a.soriano@gmail.com>
2023-03-13 18:43:24 -0700

    tree updates with tree_prog from mainpage

--- src/Components/Tree/Tree.js
+++ src/Components/Tree/Tree.js
@@ -4,62 +4,54 @@ import Class3 from "./images/photo3.png";
 import Class4 from "./images/photo4.png";
 import Class5 from "./images/photo5.png";
 import "./Tree.css"
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function Tree () {   
-    let img = null;
-    const [active, setActive] = useState(0);
-  
-    const handleChangeActive = () => {
-    setActive((previousStar) => (previousStar + 1) % 5);
-};
+function Tree (props) {   
+  const [img, setImage] = useState(null);
 
-    if (active===0) {
-      img = (
+  useEffect(() => {
+    let tree_prog = props.prog % 5;
+    if (tree_prog===0) {
+      setImage(
         <img
           className="photo1"
           src={Class1}
           alt="photo 1"
-          onClick={() => handleChangeActive()}
         />
       );
-    } else if (active===1) {
-      img = (
+    } else if (tree_prog===1) {
+      setImage(
         <img
           className="photo2"
           src={Class2}
           alt="photo 2"
-          onClick={() => handleChangeActive()}
         />
       );
-    } else if (active===2) {
-      img = (
+    } else if (tree_prog===2) {
+      setImage(
         <img
           className="photo3"
           src={Class3}
           alt="photo 3"
-          onClick={() => handleChangeActive()}
         />
       );
-    } else if (active===3) {
-      img = (
+    } else if (tree_prog===3) {
+      setImage(
         <img
           className="photo4"
           src={Class4}
           alt="photo 4"
-          onClick={() => handleChangeActive()}
         />
       );
-    } else if (active===4) {
-      img = (
+    } else if (tree_prog===4) {
+      setImage(
         <img
           className="photo5"
           src={Class5}
           alt="photo 5"
-          onClick={() => handleChangeActive()}
         />
       );
-    } 
+    } })
   
     return (
       <>
